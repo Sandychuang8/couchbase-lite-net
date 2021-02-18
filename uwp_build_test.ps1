ca8e1e42b4fb2ba3fa304334c31a8346fb20f8fb
Sandy <sandy.chuang@couchbase.com>
2021-02-18 14:37:31 -0800

    Fetch LiteCore instead build

--- uwp_build_test.ps1
+++ uwp_build_test.ps1
@@ -1,14 +1,9 @@
 # UWP PR Validation
 
-# Build LiteCore
-New-Item -Type Directory vendor/couchbase-lite-core/build_cmake/x86/RelWithDebInfo
-Push-Location vendor/couchbase-lite-core/build_cmake/x86/RelWithDebInfo
-New-Item -Type File LiteCore.dll
-New-Item -Type File LiteCore.pdb
-Pop-Location
-
-New-Item -Type Directory vendor/couchbase-lite-core/build_cmake/x64
-Push-Location vendor/couchbase-lite-core/build_cmake/x64
-cmake -G "Visual Studio 15 2017 Win64" ..\..
-cmake --build . --target LiteCore --config RelWithDebInfo
-Pop-Location
+# Fetch LiteCore
+pushd vendor/couchbase-lite-core
+$sha=$(& 'C:\Program Files\Git\bin\git.exe' rev-parse HEAD)
+popd
+cd src
+$NEXUS_REPO='http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore/'
+build/do_fetch_litecore.ps1 -DebugLib -Variants windows-win32,windows-win64,windows-arm -NexusRepo $NEXUS_REPO
