c80a3efdcfb306d2d1b5adb94d8ccf6d095dc880
Sandy <sandy.chuang@couchbase.com>
2021-02-18 15:06:40 -0800

    Update fetch LiteCore

--- uwp_build_test.ps1
+++ uwp_build_test.ps1
@@ -1,12 +1,12 @@
 # UWP PR Validation
 
 # Submodule
-git submodule update --init --recursive
+git submodule update --init # recursive not needed here  --recursive
 
 # Fetch LiteCore
 pushd vendor/couchbase-lite-core
-$sha=$(& 'C:\Program Files\Git\bin\git.exe' rev-parse HEAD)
+$sha=$(& git rev-parse HEAD)
 popd
 cd src
 $NEXUS_REPO='http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore/'
-build/do_fetch_litecore.ps1 -DebugLib -Variants windows-win32,windows-win64,windows-arm -NexusRepo $NEXUS_REPO
+build/do_fetch_litecore.ps1 -Variants windows-win32,windows-win64 -NexusRepo $NEXUS_REPO -Sha $sha
