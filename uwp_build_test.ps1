bbea6af4d300314424dfd305200f28967f7029ee
Sandy <sandy.chuang@couchbase.com>
2021-02-18 15:08:52 -0800

    No sha needed?

--- uwp_build_test.ps1
+++ uwp_build_test.ps1
@@ -4,9 +4,6 @@
 git submodule update --init # recursive not needed here  --recursive
 
 # Fetch LiteCore
-pushd vendor/couchbase-lite-core
-$sha=$(& git rev-parse HEAD)
-popd
 cd src
 $NEXUS_REPO='http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore/'
-build/do_fetch_litecore.ps1 -Variants windows-win32,windows-win64 -NexusRepo $NEXUS_REPO -Sha $sha
+build/do_fetch_litecore.ps1 -Variants windows-win32,windows-win64 -NexusRepo $NEXUS_REPO
