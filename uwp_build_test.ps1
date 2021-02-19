2d2ad9618cd7fd900301870bfb66d59458da8ac1
Sandy <sandy.chuang@couchbase.com>
2021-02-18 22:38:08 -0800

    Build LiteCore instead fetch LiteCore

--- uwp_build_test.ps1
+++ uwp_build_test.ps1
@@ -1,12 +1,26 @@
 # UWP PR Validation
 
+git submodule update --init --recursive
+
+New-Item -Type Directory vendor/couchbase-lite-core/build_cmake/x86_store/RelWithDebInfo
+        Push-Location vendor/couchbase-lite-core/build_cmake/x86_store/RelWithDebInfo
+        New-Item -Type File LiteCore.dll
+        New-Item -Type File LiteCore.pdb
+        Pop-Location
+
+        New-Item -Type Directory vendor/couchbase-lite-core/build_cmake/x64_store
+        Push-Location vendor/couchbase-lite-core/build_cmake/x64_store
+        cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 ..\..
+        cmake --build . --target LiteCore --config RelWithDebInfo
+        Pop-Location
+
 # Submodule
-git submodule update --init # (--recursive) recursive not needed here 
+#git submodule update --init # (--recursive) recursive not needed here 
 
 # Fetch LiteCore
-pushd vendor/couchbase-lite-core
-$sha=$(& git rev-parse HEAD)
-popd
-cd src
-$NEXUS_REPO='http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore/'
-build/do_fetch_litecore.ps1 -Variants windows-win32,windows-win64 -NexusRepo $NEXUS_REPO -Sha $sha
+#pushd vendor/couchbase-lite-core
+#$sha=$(& git rev-parse HEAD)
+#popd
+#cd src
+#$NEXUS_REPO='http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore/'
+#build/do_fetch_litecore.ps1 -Variants windows-win32,windows-win64 -NexusRepo $NEXUS_REPO -Sha $sha
